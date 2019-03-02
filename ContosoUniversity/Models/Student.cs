6a78b1f3210ca4c30e7114ad27d526c40e47a5e3
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2019-03-01 18:56:10 -0600

    Add seeder file

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -10,7 +10,7 @@ namespace ContosoUniversity.Models
     {
         public int Id { get; set; }
         public string LastName { get; set; }
-        public string FirstName { get; set; }
+        public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
         //One to many relationship:
         public virtual ICollection<Enrollment> Enrollments { get; set; }
